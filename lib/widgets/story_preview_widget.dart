91580d9fe338d533685ffa46f992c1e97042df71
Michal Yashpe <michal@yashpe.com>
2020-02-02 11:22:33 +0200

    fix image layout in story preview

--- lib/widgets/story_preview_widget.dart
+++ lib/widgets/story_preview_widget.dart
@@ -6,7 +6,6 @@ import 'package:daff_app/widgets/story_tags_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-double minStoryHeight = 100.0;
 
 List<Widget> buildStoryPreviewList(List<Story> stories, BuildContext context, {bool tagView = false}){
   List<Widget> storyPreviewList = List<Widget>(); 
@@ -23,14 +22,14 @@ Widget buildStoryPreviewWidget(Story story, BuildContext context, {bool tagView 
       borderRadius: BorderRadius.all(Radius.circular(5.0)),
       color: Colors.grey[100],
     ),
-    constraints: BoxConstraints(minHeight: minStoryHeight + 10.0),
+    // constraints: BoxConstraints(maxHeight: 250.0),
     padding: EdgeInsets.all(5.0),
     child: GestureDetector(
       onTap: () {
         Provider.of<StoryModel>(context, listen: false).initialize(story.id);
         Navigator.of(context).pushNamed(StoryScreen.routeName,);
       },
-      child: Row(
+      child: IntrinsicHeight(child: Row(
       children: <Widget>[
         Expanded(
           flex: 2,
@@ -42,48 +41,43 @@ Widget buildStoryPreviewWidget(Story story, BuildContext context, {bool tagView 
           child: _buildStoryInfo(story, context, tagView: tagView)
         )
         
-  ],)));
+  ],))));
 }
 
 Widget _buildStoryImage(Story story, BuildContext context){
-  double width = MediaQuery.of(context).size.width / 2;
   return  Container(
     decoration: BoxDecoration(
+      color: Colors.grey[100],
       borderRadius: BorderRadius.all(Radius.circular(5.0)),
-    ),
-    constraints: BoxConstraints( maxWidth: width, maxHeight: width, minHeight:  minStoryHeight),
-    width: width,
-    child:  ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(5.0)),
-        child: Align(
-          alignment: Alignment.bottomRight,
-          heightFactor: 0.5,
-          widthFactor: 0.5,
-          child: Image.network(
+      image: DecorationImage(
+        fit: BoxFit.cover,
+        image: NetworkImage(
             story.imageUrl,
-            fit: BoxFit.fitHeight
-          ),
-        ),
-      )
+          ),)
+    ),
+    child: null
     
   );
 }
 
 Widget _buildStoryInfo(Story story, BuildContext context, {bool tagView = false}){
   return Container( 
-    constraints: BoxConstraints(minHeight: minStoryHeight),
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Wrap(
-          crossAxisAlignment: WrapCrossAlignment.center,
-          children: <Widget>[
-            Text(story.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)), 
-            buildEditerPickMedalWidget(story), 
-            Text(" " + story.author.name , 
-              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold)
-            ),
-          ],),
+        Expanded(
+          child: Wrap(
+            spacing: 3.0,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: <Widget>[
+              Text(story.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0,), ), 
+              buildEditerPickMedalWidget(story), 
+              Text(story.author.name, 
+                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold, ),
+              ),
+            ],
+          )
+        ),
         Text(story.readingDurationString),
         buildStoryTagsWidget(story.tags, context, tagView: tagView),
         Text(story.dateFormatted)

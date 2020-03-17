0db041ea1ffa121cc960f4482c5e1d8e1bebed6f
Michal Yashpe <michal@yashpe.com>
2020-03-17 13:38:39 +0200

    firebase commented out`

--- lib/helpers/firebase_api.dart
+++ lib/helpers/firebase_api.dart
@@ -1,5 +1,5 @@
 
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -9,33 +9,33 @@ String notificationUrl = '';
 class FirebaseAPI extends ChangeNotifier {
   bool isLoading = false;
   String deviceToken = "";
-  FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
+  // FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
   // String url;
 
   void initialize(){
-    initFirebase();
-    initDeviceToken();
+    // initFirebase();
+    // initDeviceToken();
   }
-  void initFirebase() {
-  _firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) {
-        print('on message $message');
-        showStory(message);
-        return;
-      },
-      onResume: (Map<String, dynamic> message) {
-        print('on resume $message');
-        showStory(message);
-        return;
+  // void initFirebase() {
+  // _firebaseMessaging.configure(
+  //     onMessage: (Map<String, dynamic> message) {
+  //       print('on message $message');
+  //       showStory(message);
+  //       return;
+  //     },
+  //     onResume: (Map<String, dynamic> message) {
+  //       print('on resume $message');
+  //       showStory(message);
+  //       return;
 
-      },
-      onLaunch: (Map<String, dynamic> message) {
-        print('on launch $message');
-        showStory(message);
-        return;
-      },
-    );
-  }
+  //     },
+  //     onLaunch: (Map<String, dynamic> message) {
+  //       print('on launch $message');
+  //       showStory(message);
+  //       return;
+  //     },
+  //   );
+  // }
 
   void showStory(Map<String, dynamic> message){
         notificationUrl = message['data']['url'];
@@ -44,13 +44,13 @@ class FirebaseAPI extends ChangeNotifier {
         _launchURL(url: message['data']['url']);
   }
 
-  void initDeviceToken(){
-    isLoading = true;
-    _firebaseMessaging.getToken().then((token){
-      deviceToken = token;
-    });
-    isLoading = false;
-  }
+  // void initDeviceToken(){
+  //   isLoading = true;
+  //   _firebaseMessaging.getToken().then((token){
+  //     deviceToken = token;
+  //   });
+  //   isLoading = false;
+  // }
 
   void _launchURL({String url = 'https://daff.co.il/users/sign_up'}) async {
     if (await canLaunch(url)) {
